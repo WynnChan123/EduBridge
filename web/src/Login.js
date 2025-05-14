d6b75df33c64b1db7ee7fdc7701e917928dcc396
wynn12345 <wynnchan20@gmail.com>
2025-05-14 22:36:40 +0800

    add tailwind css config

---
+++ web/src/Login.js
@@ -0,0 +1,13 @@
+
+export default function Login() {
+  return (
+    <div>
+      <div className="bg-white w-1/2 flex items-center justify-center">
+          <h1 className="text-xl font-bold justify-center">Sign In To EduBridge</h1>
+      </div>
+      <div className="flex w-1/2 h-screen">
+        <div className="bg-[#2B2F4B] w-1/2"></div>
+      </div>
+    </div>
+  );
+}
---
+++ web/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
